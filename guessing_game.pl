5ae01f1d063fbf96988593f8b220383fca974b26
Ben Phillips <pben012@gmail.com>
2024-07-02 12:25:05 +1000

    polished guessing game

--- guessing_game.pl
+++ guessing_game.pl
@@ -3,12 +3,20 @@ use strict;
 use warnings;
 use v5.10.0;
 
-my $randNum = int(rand(100));
+my $randNum = int(rand(100) + 1);
 my $num_guesses = 0;
 
+say "I am thinking of a random number between 1 and 100.";
+say "Guess what number I am thinking of.....";
+
 while (my $input = <STDIN>) {
-    chomp $input;
-    
+    say "Enter another guess: ";
+
+    if ($input < 1 || $input > 100) {
+        say "Number must be between 1 and 100.";
+        next;
+    }
+
     if ($input == $randNum) {
         $num_guesses++;
         say "You guessed my number!!";
